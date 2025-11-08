e15b28b530ba6a0b9f8f2c28dc6b70587e46c4ef
Subhrajit Maitra <smaitra006@gmail.com>
2025-11-08 21:47:43 +0530

    Update leetcodePOTD

--- POTD/leetcodePOTD.cpp
+++ POTD/leetcodePOTD.cpp
@@ -7919,3 +7919,18 @@ private:
     return true;
   }
 };
+
+class Solution
+{
+public:
+  int minimumOneBitOperations(int n)
+  {
+    int res = 0;
+    while (n)
+    {
+      res ^= n;
+      n >>= 1;
+    }
+    return res;
+  }
+};
