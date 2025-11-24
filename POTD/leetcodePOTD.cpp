db8035a33b7ac6b0d0f4a11f862deee1904abb14
Subhrajit Maitra <smaitra006@gmail.com>
2025-11-24 20:35:17 +0530

    Update leetcodePOTD

--- POTD/leetcodePOTD.cpp
+++ POTD/leetcodePOTD.cpp
@@ -8165,3 +8165,19 @@ public:
     return cnt;
   }
 };
+
+class Solution
+{
+public:
+  vector<bool> prefixesDivBy5(vector<int> &nums)
+  {
+    vector<bool> ans;
+    int val = 0;
+    for (int bit : nums)
+    {
+      val = (val * 2 + bit) % 5;
+      ans.push_back(val == 0);
+    }
+    return ans;
+  }
+};
