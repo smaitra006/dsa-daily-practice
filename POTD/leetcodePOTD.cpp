2bd5175ad46b6cf91de39a0ac00415aff8e2c769
Subhrajit Maitra <smaitra006@gmail.com>
2025-11-25 20:22:24 +0530

    Update leetcodePOTD

--- POTD/leetcodePOTD.cpp
+++ POTD/leetcodePOTD.cpp
@@ -8181,3 +8181,23 @@ public:
     return ans;
   }
 };
+
+class Solution
+{
+public:
+  int smallestRepunitDivByK(int k)
+  {
+    if (k == 1)
+      return 1;
+    if (k % 2 == 0 || k % 5 == 0)
+      return -1;
+    int x = 0;
+    for (int i = 1; i <= k; i++)
+    {
+      x = (x * 10 + 1) % k;
+      if (x == 0)
+        return i;
+    }
+    return -1;
+  }
+};
