d2762c846e61e206998ba366986801be8be2bd2c
Subhrajit Maitra <smaitra006@gmail.com>
2025-11-29 19:58:05 +0530

    Update leetcodePOTD

--- POTD/leetcodePOTD.cpp
+++ POTD/leetcodePOTD.cpp
@@ -8287,3 +8287,17 @@ public:
     return count;
   }
 };
+
+class Solution
+{
+public:
+  int minOperations(vector<int> &nums, int k)
+  {
+    int sum = 0;
+    for (auto x : nums)
+    {
+      sum += x;
+    }
+    return sum % k;
+  }
+};
